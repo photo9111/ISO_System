6b022fcdd1c750585c08d98ed570c2f99b4161b9
12612 <1251220183@qq.com>
2026-07-01 18:23:52 +0800

    fix: change storage paths from D:\ to relative Data\ path (D drive may not exist)

--- ISO11820/obj/Debug/net8.0-windows/ISO11820.AssemblyInfo.cs
+++ ISO11820/obj/Debug/net8.0-windows/ISO11820.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ISO11820")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+223a11a07354fe018d36b9b72090527eb5c989de")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8508114fff161ef368ed1c65a1f16ff8b2564c26")]
 [assembly: System.Reflection.AssemblyProductAttribute("ISO11820")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ISO11820")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
