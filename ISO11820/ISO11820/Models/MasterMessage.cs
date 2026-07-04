9cf294ddd0e4bc717aaa2ab759de32375bbba0da
Q-Y06 <2844236358@qq.com>
2026-07-04 14:42:35 +0800

    chore: remove merged ISO11820/ directory from repo

--- ISO11820/ISO11820/Models/MasterMessage.cs
+++
@@ -1,7 +0,0 @@
-namespace ISO11820.Models;
-
-public class MasterMessage
-{
-    public string Time { get; set; } = string.Empty;
-    public string Message { get; set; } = string.Empty;
-}
--- ISO11820/ISO11820/Models/Operator.cs
+++
@@ -1,9 +0,0 @@
-namespace ISO11820.Models;
-
-public class Operator
-{
-    public string UserId { get; set; } = string.Empty;
-    public string Username { get; set; } = string.Empty;
-    public string Password { get; set; } = string.Empty;
-    public string Role { get; set; } = string.Empty;
-}
